90713a84c3dddf1faac8c6b8182ddc45ec49ec87
ABHISHEK TRIPATHI <abhishektripathi@ABHISHEKs-MacBook-Air-2.local>
2025-12-26 17:34:11 +0530

    Fix render deployment

--- app.py
+++ app.py
@@ -207,6 +207,10 @@ def shared_download(token):
 # --------------------------------
 # DEPLOY
 # --------------------------------
+from waitress import serve
+import os
+
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Render gives PORT automatically
+    serve(app, host="0.0.0.0", port=port)
+
